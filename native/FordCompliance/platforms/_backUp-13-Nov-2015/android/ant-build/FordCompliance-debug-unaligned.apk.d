67ac34b3d7d240f5466ecbd6a4769aa45eb48516
Deepa-Ck <deepack.2010@gmail.com>
2016-06-23 20:40:34 +0530

    Deleted all unwanted files

--- native/FordCompliance/platforms/_backUp-13-Nov-2015/android/ant-build/FordCompliance-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-D:\android_workspace\FordCompliance\platforms\android\ant-build\FordCompliance-debug-unaligned.apk : \
-D:\android_workspace\FordCompliance\platforms\android\ant-build\FordCompliance.ap_ \
-D:\android_workspace\FordCompliance\platforms\android\ant-build\classes.dex \
-D:\android_workspace\FordCompliance\platforms\android\CordovaLib\ant-build\classes.jar \
--- native/FordCompliance/platforms/_backUp-13-Nov-2015/ios/www/app/controllers/gifts-favors.js
+++
@@ -1,9 +0,0 @@
-angular.module('fcApp').controller('GiftsFavorsController', ['$scope', '$routeParams',
-function($scope, $routeParams) {
-	var appJson = JSON.parse(localStorage.getItem('appJson'));
-
-	$scope.vModel = {
-		items : appJson.findBy('pageKey', $routeParams.pageKey).items,
-		title : appJson.findBy('pageKey', $routeParams.pageKey).title
-	};
-}]); 
